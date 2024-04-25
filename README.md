c660ea4ce47e01bb5bf19d478ff0434d3cb5d4a6
mazomenos31 <gustbetancourt@hotmail.com>
2024-04-25 16:17:02 -0700

    initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Flexlogic_Onboarding
+
+Comandos:
+
+npm i
+
+npm run dev
+
+#Prisma:
+
+-Migrar modelo
+npx prisma db push
+
---
+++ flexlogic_onboarding/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p>pagina Inicial</p>
+      <button className="btn btn-primary">Button</button>
+    </main>
+  );
+}
