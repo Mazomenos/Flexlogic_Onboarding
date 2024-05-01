4c046e6c683edfaefe5920b954a81da35960687e
mctuliomontoya <mctulio.2@gmail.com>
2024-05-01 12:44:35 -0700

    styles: add company colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,35 @@ const config: Config = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#C1E2F4",
+          secondary: "#8CC5E4",
+          accent: "#FFCF52",
+          neutral: "#242424",
+          "base-100": "#FFFFFF",
+          "base-200": "#F6F6F6",
+          "base-300": "#E3E3E3",
+          info: "#A6BFFF",
+          success: "#BCDBA9",
+          warning: "#FAE5AE",
+          error: "#DA9999",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "0.2rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
 export default config;
