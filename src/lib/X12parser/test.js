5659689e2f81bc27885c50fd3aaf01a556e484d0
mazomenos31 <gustbetancourt@hotmail.com>
2024-05-02 17:30:56 -0700

    Added Parser in lib folder with essencial files and was able to allow storing whitespaces

---
+++ src/lib/X12parser/test.js
@@ -0,0 +1,20 @@
+import { createReadStream } from 'fs';
+
+import X12parser from './lib/X12parser.js';
+
+// Create a new parser
+const myParser = new X12parser();
+myParser.on('error', err => {
+    console.error(err);
+})
+
+// Create a read stream from a file
+const ediFile = createReadStream('./edi.txt')
+ediFile.on('error', err => {
+    console.error(err);
+})
+
+// Handle events from the parser
+ediFile.pipe(myParser).on('data', data => {
+    console.log(data);
+})
