a598b5cad953f5334b25ce82d552130c962c8725
mctuliomontoya <mctulio.2@gmail.com>
2024-05-13 15:01:09 -0700

    style: changed header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
 export default function Header({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="pt-4">
-            <div className="bg-white mr-6 ml-6 rounded-md h-20 flex justify-center items-center">
-                <div className="text-4xl font-bold"> {children} </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-white shadow-md h-20 flex justify-center items-center">
+      <div className="text-4xl font-bold"> {children} </div>
+    </div>
+  );
 }
