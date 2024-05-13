8236f4d5e7cd5cc471001119dfeb905e5440ede9
mctuliomontoya <mctulio.2@gmail.com>
2024-05-13 15:45:37 -0700

    style: add font sans serif

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,8 +5,8 @@ import Header from "@/components/Header";
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
-      <main className="bg-base-200 h-screen flex flex-col max-h-dvh">
-        <Header> hola </Header>
+      <main className="bg-base-200 font-sans-serif h-screen flex flex-col tracking-normal font-extrabold max-h-dvh">
+        <Header> Partners list </Header>
         <Container>{children}</Container>
       </main>
     </React.Fragment>
