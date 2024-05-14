60d36540ebbb4a8ae6ac2eb018bdc59742fbda61
mctuliomontoya <mctulio.2@gmail.com>
2024-05-14 12:06:26 -0700

    fix: change styles to fix issues
    Changed drop shadow color to match primary-content
    Delete ring attributes
    Delete black: properties.

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -7,7 +7,7 @@ export default function ListItem({ children, id }: IListItem) {
   return (
     <li
       key={id}
-      className="flex bg-base-200 justify-between items-center flex-row place-items-start mx-1 my-2 w-full px-8 shadow-lg shadow-neutral-300 ring-1 ring-base-300 hover:bg-base-300 border border-transparent rounded py-6 dark:hover:bg-gray-700"
+      className="flex bg-base-200 justify-between items-center flex-row place-items-start mx-1 my-4 w-full px-8 shadow-md  shadow-primary-content/20 hover:bg-base-300 border border-transparent rounded py-6"
     >
       {children}
     </li>
