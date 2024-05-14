e3d1ce5cc09fec9db76c4f45c6fca340ff3d474d
danielaruiz1 <daniela.ruizgar1@gmail.com>
2024-05-14 16:27:56 -0700

    created parseEDIfileTest function and a test button

---
+++ src/components/Parser/parserTest.tsx
@@ -0,0 +1,31 @@
+import { createReadStream } from "fs";
+import { ParseEDIfileTest } from "../../libs/X12parser/lib/parseEDIfileTest.js";
+
+export default function ParserTest() {
+  async function upload(data: FormData) {
+    "use server";
+
+    const file: File | null = data.get("file") as unknown as File;
+
+    if (!file) {
+      throw new Error("No file was uploaded");
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    ParseEDIfileTest(file.name);
+
+    return { success: true };
+  }
+
+  return (
+    <main>
+      <h1>EDI Parser</h1>
+      <form action={upload}>
+        <input type="file" name="file" />
+        <input type="submit" value="Upload" />
+      </form>
+    </main>
+  );
+}
---
+++ src/libs/X12parser/lib/parseEDIfileTest.js
@@ -0,0 +1,22 @@
+import { createReadStream } from "fs";
+
+import X12parser from "@/libs/X12parser/lib/X12parser";
+
+export function ParseEDIfileTest(filename) {
+  // Create a new parser
+  const myParser = new X12parser("ascii", "~", ">", "*", "U");
+  myParser.on("error", (err) => {
+    console.error(err);
+  });
+
+  // Create a read stream from a file
+  const ediFile = createReadStream(filename);
+  ediFile.on("error", (err) => {
+    console.error(err);
+  });
+
+  // Handle events from the parser
+  ediFile.pipe(myParser).on("data", (data) => {
+    console.log(data);
+  });
+}
