73c3992ef3a292b48554909870569923a45b0c27
NovoxMx <A00835132@tec.mx>
2024-05-14 15:44:18 -0700

    refactor: Replace SaveButton with GenericButton

---
+++ src/components/GenericButton.tsx
@@ -0,0 +1,7 @@
+export default function GenericButton({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
+    return (
+        <button className="size-1/12 h-10 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md" onClick={onClick}>
+            <div className="flex justify-center items-center"> {children} </div>
+        </button>
+    );
+}
--- src/components/SaveButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function SaveButton({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
-    return (
-        <button className="size-1/12 h-10 border bg-primary text-black hover:bg-white hover:text-[#99CEFF] hover:border-primary hover:border-2 font-bold rounded-md" onClick={onClick}>
-            <div className="flex justify-center items-center"> {children} </div>
-        </button>
-    );
-}
