dbb87627cbcd9afe78ee449cfff8dbfa861f536f
mctuliomontoya <mctulio.2@gmail.com>
2024-05-16 17:32:13 -0700

    fix: change br color

--- src/components/BrakeRule.tsx
+++ src/components/BrakeRule.tsx
@@ -1,5 +1,3 @@
 export default function BrakeRule() {
-    return (
-        <hr className="my-6 ml-1 h-0.5 border-t-0 bg-neutral-200"/>
-    );
+  return <hr className="my-6 ml-1 h-0.5 border-t-0 bg-base-300" />;
 }
