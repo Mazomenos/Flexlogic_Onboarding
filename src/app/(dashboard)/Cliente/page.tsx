3b35e84268316534d629366333c4defbc7c6d56c
mctuliomontoya <mctulio.2@gmail.com>
2024-05-16 17:32:26 -0700

    feat: add basic client page

--- src/app/(dashboard)/Cliente/page.tsx
+++ src/app/(dashboard)/Cliente/page.tsx
@@ -1,8 +1,36 @@
+import AddButton from "@/components/AddButton";
+import BrakeRule from "@/components/BrakeRule";
+import ListItem from "@/components/ListItem";
+import Badges from "./components/Badges";
+
+type Partnership = {
+  id: number;
+  name: string;
+  status: string;
+};
+
 export default function Home() {
+  // Aquí iría la llamada a las partnerships
+  const partnerships: Partnership[] = [
+    { id: 1, name: "Amazon", status: "Complete" },
+    { id: 2, name: "Walmart", status: "In Process" },
+    { id: 3, name: "Partner X", status: "Complete" },
+    { id: 4, name: "Partner y", status: "Failed" },
+  ];
+
   return (
     <main>
-      <p>Cliente</p>
-      <button className="btn btn-primary">Button</button>
+      <div className="w-full justify-end flex">
+        {" "}
+        <AddButton> + Partnership </AddButton>
+      </div>
+      <BrakeRule />
+      {partnerships.map((partnership, index) => (
+        <ListItem key={index} path={partnership.name}>
+          <p>{partnership.name} </p>
+          <Badges status={partnership.status} />
+        </ListItem>
+      ))}
     </main>
   );
 }
