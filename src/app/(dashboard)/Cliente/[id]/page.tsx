4c01e94a463a77575d36374bce849fa1cb5d57ce
NovoxMx <A00835132@tec.mx>
2024-05-16 18:05:17 -0700

    feat: add client dynamics router pages

---
+++ src/app/(dashboard)/Cliente/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main>
+        <p>Cliente EDI DOCS </p>
+        <button className="btn btn-primary">Button</button>
+      </main>
+    );
+  }
+  
