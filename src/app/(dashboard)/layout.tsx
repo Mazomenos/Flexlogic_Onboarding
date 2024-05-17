50d7850a5c07f693f8b533ac94651b62928c6cd6
NovoxMx <A00835132@tec.mx>
2024-05-17 11:51:34 -0700

    feat: refactor layout component and improve partner page title handling with a function

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,32 +1,14 @@
 "use client";
 import React from "react";
-import { useState } from "react";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
 import { usePathname } from "next/navigation";
-import { map } from "zod";
+import {getPageName} from './utils/getPageName';
 
 function Layout({ children }: { children: React.ReactNode }) {
-  const currentPath = usePathname();
-  const [partners, setPartners] = useState([
-    { id: 1, image: "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/mu6zkbsr9lfqeq9nvwls.png", companyName: "Amazon", ediDoc: ["850", "855", "810", "860"], visible: true },
-    { id: 2, image: "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/dgf2ebuyc3xbrohuwzep.svg", companyName: "Walmart", ediDoc: ["850", "855", "810", "860"], visible: true },
-    { id: 3, image: "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/oznp9xnvnzeclt8snqtr.png", companyName: "Autozone", ediDoc: ["850", "855", "810", "860"], visible: true }
-  ]);
 
-  let pageName = "";
-
-  partners.map((partner) => {
-    if (currentPath === ("/Cliente/" + partner.id)) {
-      pageName = partner.companyName;
-    }
-  });
-
-  if (currentPath === "/Admin") {
-    pageName = "Partners";
-  } else if (currentPath === "/Cliente") {
-    pageName = "PartnerShip";
-  }
+  let currentPath = usePathname();
+  const pageName = getPageName(currentPath);
 
   return (
     <React.Fragment>
