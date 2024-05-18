c57eb99c98f2648b93076a054dd097b9c8840e58
mctuliomontoya <mctulio.2@gmail.com>
2024-05-17 17:47:20 -0700

    remove old font

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,20 +3,18 @@ import React from "react";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
 import { usePathname } from "next/navigation";
-import {getPageName} from './utils/getPageName';
+import { getPageName } from "./utils/getPageName";
+import Provider from "@/components/Provider";
 
 function Layout({ children }: { children: React.ReactNode }) {
-
   let currentPath = usePathname();
   const pageName = getPageName(currentPath);
 
   return (
-    <React.Fragment>
-      <main className="bg-base-200 text-primary-content font-sans-serif h-screen flex flex-col tracking-normal font-extrabold max-h-dvh">
-        <Header>{pageName}</Header>
-        <Container>{children}</Container>
-      </main>
-    </React.Fragment>
+    <main className="bg-base-200 text-primary-content h-screen flex flex-col max-h-dvh">
+      <Header>{pageName}</Header>
+      <Container>{children}</Container>
+    </main>
   );
 }
 
