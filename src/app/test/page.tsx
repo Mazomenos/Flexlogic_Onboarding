e8bba6cdd90d196ebe2ea3ea138bf93e632485da
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-05-17 18:14:23 -0700

    min max basico

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,50 @@
 "use client"
-import { z } from "zod";
-import { useState } from "react";
+import { string, z } from "zod";
+import { use, useState } from "react";
 
 export default function Home() {
     const [inputString, setInputString] = useState()
+    const [minString, setMinString] = useState()
+    const [maxString, setMaxString] = useState()
     const [errorLog, setErrorLog] = useState();
 
+    const max = (flag: boolean) => {
+        const max = Number(maxString)
+        if (flag === true && inputString.length > max) {
+            flag = false
+            console.log("El valor debe ser maximo " + max)
+            return
+        }
+        console.log("Validado por max")
+        return
+    }
+
+    const min = () => {
+        const required = "M"
+        const min = Number(minString)
+        if (min > inputString.length) {
+            if (inputString.length === 0) {
+                if (required === "M") {
+                    max(false)
+                    console.log("El campo es mandatorio")
+                    return
+                }
+                else {
+                    max(false)
+                    console.log("Pasa por Opt")
+                    return
+                }
+            }
+            else {
+                max(false)
+                console.log("El valor debe ser minimo " + min)
+                return
+            }
+        }
+        max(true)
+        console.log("Validado por min")
+        return
+    }
 
     const alphanumericTest = () => {
         const regexSchema = z.string().regex(new RegExp('^[^*~:]+$'), 'String must not have delimiters inside them')
@@ -49,19 +88,26 @@ export default function Home() {
         console.log(result)
     }
 
-
-
     return (
         <main>
-            <p></p>
-            <input type="text" onChange={(e) => setInputString(e.target.value)}></input>
-            <button className="btn btn-primary" onClick={alphanumericTest}>AN</button>
-            <button className="btn btn-warning" onClick={numberTest}>N</button>
-            <button className="btn btn-primary" onClick={decimalTest}>R</button>
-            <button className="btn btn-warning" onClick={idTest}>ID</button>
-            <button className="btn btn-primary" onClick={tmTest}>TM</button>
-            <button className="btn btn-warning" onClick={dtTest}>DT</button>
-            <button className="btn btn-primary" onClick={bTest}>B</button>
+            <p>Test</p>
+            <input type="text" placeholder="Value" onChange={(e) => setInputString(e.target.value)}></input>
+            <div>
+                <input type="text" placeholder="Min" onChange={(e) => setMinString(e.target.value)}></input>
+                <input type="text" placeholder="Max" onChange={(e) => setMaxString(e.target.value)}></input>
+            </div>
+            <div>
+                <button className="btn btn-primary" onClick={min}>Min/Max</button>
+            </div>
+            <div>
+                <button className="btn btn-primary" onClick={alphanumericTest}>AN</button>
+                <button className="btn btn-warning" onClick={numberTest}>N</button>
+                <button className="btn btn-primary" onClick={decimalTest}>R</button>
+                <button className="btn btn-warning" onClick={idTest}>ID</button>
+                <button className="btn btn-primary" onClick={tmTest}>TM</button>
+                <button className="btn btn-warning" onClick={dtTest}>DT</button>
+                <button className="btn btn-primary" onClick={bTest}>B</button>
+            </div>
         </main>
     );
 }
