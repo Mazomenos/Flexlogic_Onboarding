e8a7aa3d505a281128209b8eb4c9c1b3e706f29b
mctuliomontoya <mctulio.2@gmail.com>
2024-05-18 23:21:47 -0700

    fix: change component h to fill available space

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-6 flex-auto flex flex-col">
-      <div className="p-4 flex-1 border-2 bg-base-100 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] rounded-lg border-transparent">
+    <div className="p-6 h-full  bg-base-200 max-h-dvh ">
+      <div className="p-4 h-full border-2 bg-base-100 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] rounded-lg border-transparent ">
         {children}
       </div>
     </div>
