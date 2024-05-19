4ef8d26a867fb4d58a6b758958f9a4663551b5c9
mctuliomontoya <mctulio.2@gmail.com>
2024-05-18 23:18:26 -0700

    fix: change styling and width to support overflow scrolling

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -14,8 +14,8 @@ export default function ListItem({ children, id, path }: IListItem) {
       key={id}
       className={
         path
-          ? "cursor-pointer dark:bg-red-800 flex bg-base-100 justify-between items-center flex-row place-items-start mx-1 my-4 w-full px-8 shadow-[0px_0px_10px_1px_#00000024] 0 hover:bg-base-200 border-1 border-base-200 rounded-xl text-xl py-6"
-          : "flex bg-base-200 justify-between items-center flex-row place-items-start mx-1 my-4 w-full px-8 shadow-md  shadow-primary-content/20 border border-transparent rounded py-6"
+          ? "cursor-pointer dark:bg-red-800 w-[97%] flex bg-base-100 justify-between items-center flex-row place-items-start mx-1 my-2 px-8 shadow-[0px_0px_10px_1px_#00000024] 0 hover:bg-base-200 border-1 border-base-200 rounded-xl text-xl py-6"
+          : "dark:bg-red-800 w-[97%] flex bg-base-100 justify-between items-center flex-row place-items-start mx-1 my-2 px-8 shadow-[0px_0px_10px_1px_#00000024] 0  border-base-200 rounded-xl text-xl py-6"
       }
     >
       {children}
