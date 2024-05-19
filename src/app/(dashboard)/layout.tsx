9b378fd0045f557276929d14a584f9dbab160f5f
mctuliomontoya <mctulio.2@gmail.com>
2024-05-18 23:25:21 -0700

    fix: fixed styles when a children exceeds max h
    - I fixed component parent div to be 100% - 5rem (h of header)

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,16 +4,19 @@ import Container from "@/components/Container";
 import Header from "@/components/Header";
 import { usePathname } from "next/navigation";
 import { getPageName } from "./utils/getPageName";
-import Provider from "@/components/Provider";
 
 function Layout({ children }: { children: React.ReactNode }) {
   let currentPath = usePathname();
   const pageName = getPageName(currentPath);
 
   return (
-    <main className="bg-base-200 text-primary-content h-screen flex flex-col max-h-dvh">
-      <Header>{pageName}</Header>
-      <Container>{children}</Container>
+    <main className="bg-base-200 text-primary-content h-screen">
+      <div className="h-20">
+        <Header>{pageName}</Header>
+      </div>
+      <div className="h-[calc(100vh-5rem)]">
+        <Container>{children}</Container>
+      </div>
     </main>
   );
 }
