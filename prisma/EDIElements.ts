f36158471db6b165f9d4a8c5882cde040055580d
Jaza2401 <alex.jauregui.zarate@gmail.com>
2024-05-19 20:19:01 -0700

    - Updated Schema with user and TP structure - Updated some if the models and type definitions

--- prisma/EDIElements.ts
+++ prisma/EDIElements.ts
@@ -1,7 +1,7 @@
 export const EDIElements = [
     {
         Element : '143',
-        NameElement : 'Transaction Set Identifier Code',
+        ElementName : 'Transaction Set Identifier Code',
         Type : 'Identifier (ID)',
         Min : 3,
         Max : 3,
@@ -10,7 +10,7 @@ export const EDIElements = [
     },
     {
         Element : '329',
-        NameElement : 'Transaction Set Control Number',
+        ElementName : 'Transaction Set Control Number',
         Type : 'String (AN)',
         Min : 4,
         Max : 9,
@@ -19,10 +19,122 @@ export const EDIElements = [
     },
     {
         Element : '1705',
-        NameElement : 'Implementation Convention Reference',
+        ElementName : 'Implementation Convention Reference',
         Type : 'String (AN)',
         Min : 1,
         Max : 35,
         Version : '4010'
-    }
+    },
+    {
+        Element : '373',
+        ElementName : 'Date',
+        Type : 'Date(DT)',
+        Min : 8,
+        Max : 8,
+        Version : '4010'
+    },
+    {
+        Element : '76',
+        ElementName : 'Invoice Number',
+        Type : 'String(AN)',
+        Min : 1,
+        Max : 22,
+        Version : '4010'
+    },
+    {
+        Element : '324',
+        ElementName : 'Purchase Order Number',
+        Type : 'String(AN)',
+        Min : 1,
+        Max : 22,
+        Version : '4010'
+    },
+    {
+        Element : '328',
+        ElementName : 'Release Number',
+        Type : 'String(AN)',
+        Min : 1,
+        Max : 30,
+        Version : '4010'
+    },
+    {
+        Element : '327',
+        ElementName : 'Change Order Sequence Number',
+        Type : 'String(AN)',
+        Min : 1,
+        Max : 8,
+        Version : '4010'
+    },
+    {
+        Element : '640',
+        ElementName : 'Transaction Type Code',
+        Type : 'Identifier(ID)',
+        Min : 2,
+        Max : 2,
+        Version : '4010'
+    },
+    {
+        Element : '353',
+        ElementName : 'Transaction Set Purpose Code',
+        Type : 'Identifier(ID)',
+        Min : 2,
+        Max : 2,
+        Version : '4010'
+    },
+    {
+        Element : '306',
+        ElementName : 'Action Code',
+        Type : 'Identifier(ID)',
+        Min : 1,
+        Max : 2,
+        Version : '4010'
+    },
+    {
+        Element : '1005',
+        ElementName : 'Hierarchical Structure Code',
+        Type : 'Identifier(ID)',
+        Min : 4,
+        Max : 4,
+        Version : '4010'
+    },
+    {
+        Element : '92',
+        ElementName : 'Purchase Order Type Code',
+        Type : 'Identifier(ID)',
+        Min : 2,
+        Max : 2,
+        Version : '4010'
+    },
+    {
+        Element : '367',
+        ElementName : 'Contact Number',
+        Type : 'String(AN)',
+        Min : 1,
+        Max : 30,
+        Version : '4010'
+    },
+    {
+        Element : '587',
+        ElementName : 'Acknowledge Type Code',
+        Type : 'Identifier(ID)',
+        Min : 2,
+        Max : 2,
+        Version : '4010'
+    },
+    {
+        Element : '1019',
+        ElementName : 'Invoice Type Code',
+        Type : 'Identifier(ID)',
+        Min : 3,
+        Max : 3,
+        Version : '4010'
+    },
+    {
+        Element : '1166',
+        ElementName : 'Contact Type Code',
+        Type : 'Identifier(ID)',
+        Min : 2,
+        Max : 2,
+        Version : '4010'
+    },
 ];
