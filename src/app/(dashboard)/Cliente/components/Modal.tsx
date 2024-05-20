e9aa1d83d3b6d6cf7aaff630e7bc125f1ac01b65
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 00:00:56 -0700

    test: modal test

---
+++ src/app/(dashboard)/Cliente/components/Modal.tsx
@@ -0,0 +1,77 @@
+"use client";
+import {
+  Button,
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/16/solid";
+
+export default function Modal({ children }: { children: React.ReactNode }) {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      <Button
+        onClick={open}
+        className="rounded-md bg-black/20 py-2 px-4 text-sm font-medium text-white focus:outline-none data-[hover]:bg-black/30 data-[focus]:outline-1 data-[focus]:outline-white"
+      >
+        Open dialog
+      </Button>
+
+      <Transition appear show={isOpen}>
+        <Dialog
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={close}
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 transform-[scale(95%)]"
+                enterTo="opacity-100 transform-[scale(100%)]"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 transform-[scale(100%)]"
+                leaveTo="opacity-0 transform-[scale(95%)]"
+              >
+                <DialogPanel className="w-full max-w-md rounded-xl text-black bg-white/5 p-6 backdrop-blur-2xl">
+                  <DialogTitle
+                    as="h3"
+                    className="text-base/7 font-medium text-white"
+                  >
+                    Payment successful
+                  </DialogTitle>
+                  <p className="mt-2 text-sm/6 text-white/50">
+                    Your payment has been successfully submitted. We’ve sent you
+                    an email with all of the details of your order.
+                  </p>
+                  <div className="mt-4">
+                    <Button
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      onClick={close}
+                    >
+                      Got it, thanks!
+                    </Button>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
--- src/app/(dashboard)/Cliente/docs/page.tsx
+++ src/app/(dashboard)/Cliente/docs/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import {
+  DialogPanel,
+  DialogTitle,
+  Description,
+  CloseButton,
+  Button,
+} from "@headlessui/react";
+
+import Modal from "../components/Modal";
+import MyButton from "../components/MyButton";
 export default function Home() {
-    return (
-      <main>
-        <p>Cliente docs</p>
-        <button className="btn btn-primary">Button</button>
-      </main>
-    );
-  }
+  return (
+    <>
+      <Modal>
+        <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+        <Description>This will permanently deactivate your account</Description>
+        <p>
+          Are you sure you want to deactivate your account? All of your data
+          will be permanently removed.
+        </p>
+        <CloseButton as={MyButton}>Cancel</CloseButton>
+      </Modal>
+    </>
+  );
+}
