6a842c7bb33911320322d36a3753053b80f8c08f
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 16:20:52 -0700

    fix: created better modal that handles children

--- src/app/(dashboard)/Cliente/components/Modal.tsx
+++ src/app/(dashboard)/Cliente/components/Modal.tsx
@@ -1,77 +1,25 @@
-"use client";
-import {
-  Button,
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  Transition,
-  TransitionChild,
-} from "@headlessui/react";
-import { useState } from "react";
-import { XMarkIcon } from "@heroicons/react/16/solid";
+import { Dialog, DialogPanel } from "@headlessui/react";
 
-export default function Modal({ children }: { children: React.ReactNode }) {
-  let [isOpen, setIsOpen] = useState(false);
+interface Props {
+  children: React.ReactNode;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+}
 
-  function open() {
-    setIsOpen(true);
-  }
-
-  function close() {
-    setIsOpen(false);
-  }
-
+export default function Modal({ children, setIsOpen, isOpen }: Props) {
   return (
     <>
-      <Button
-        onClick={open}
-        className="rounded-md bg-black/20 py-2 px-4 text-sm font-medium text-white focus:outline-none data-[hover]:bg-black/30 data-[focus]:outline-1 data-[focus]:outline-white"
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
       >
-        Open dialog
-      </Button>
-
-      <Transition appear show={isOpen}>
-        <Dialog
-          as="div"
-          className="relative z-10 focus:outline-none"
-          onClose={close}
-        >
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4">
-              <TransitionChild
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 transform-[scale(95%)]"
-                enterTo="opacity-100 transform-[scale(100%)]"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 transform-[scale(100%)]"
-                leaveTo="opacity-0 transform-[scale(95%)]"
-              >
-                <DialogPanel className="w-full max-w-md rounded-xl text-black bg-white/5 p-6 backdrop-blur-2xl">
-                  <DialogTitle
-                    as="h3"
-                    className="text-base/7 font-medium text-white"
-                  >
-                    Payment successful
-                  </DialogTitle>
-                  <p className="mt-2 text-sm/6 text-white/50">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
-                  <div className="mt-4">
-                    <Button
-                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
-                      onClick={close}
-                    >
-                      Got it, thanks!
-                    </Button>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
+        <div className="fixed inset-0 bg-black/30 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border border-base-200 text-primary-content bg-base-100 p-12 rounded-xl">
+            {children}
+          </DialogPanel>
+        </div>
+      </Dialog>
     </>
   );
 }
