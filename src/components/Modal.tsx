72927d6c5334e119c85d933fc1e1fef090c3d8bf
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 19:45:01 -0700

    feat: add cancel button to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import { Dialog, DialogPanel } from "@headlessui/react";
+import CloseButton from "./CloseButton";
 
 interface Props {
   children: React.ReactNode;
@@ -15,8 +16,11 @@ export default function Modal({ children, setIsOpen, isOpen }: Props) {
         className="relative z-50"
       >
         <div className="fixed inset-0 bg-black/30 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-1/2 w-2/5 h-1/2 flex flex-col items-center overflow-y-auto overscroll-none border border-base-200 text-primary-content bg-base-100 p-12 rounded-xl">
+          <DialogPanel className="relative max-w-1/2 w-2/5 h-1/2 flex flex-col items-center overflow-y-auto overscroll-none border border-base-200 text-primary-content bg-base-100 p-12 rounded-xl">
             {children}
+            <div className="absolute top-4 right-4">
+              <CloseButton onClick={() => setIsOpen(false)} />{" "}
+            </div>
           </DialogPanel>
         </div>
       </Dialog>
