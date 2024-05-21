00590d93746a2f87842e31f28fe51e3f32deb115
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 19:51:12 -0700

    style: change icons color to match content color

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,7 +1,7 @@
 export default function BackButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="p-2 rounded-full bg-base-100 text-black hover:bg-base-200 justify-center items-center shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]"
+      className="p-2 rounded-full bg-base-100 text-primary-content hover:bg-base-200 justify-center items-center shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]"
       onClick={onClick}
     >
       <svg
