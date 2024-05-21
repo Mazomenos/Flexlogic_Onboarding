cfa539caabfa6635481c50b4acd6b2e831c08e86
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 20:14:37 -0700

    change getPageName to custom react Hook

--- src/app/(dashboard)/utils/getPageName.tsx
+++ src/app/(dashboard)/utils/getPageName.tsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-function getPageName(currentPath: string ) {
-    const router = useRouter();
+function useGetPageName(currentPath: string) {
+  const router = useRouter();
 
-    const [partners] = useState([
-        { id: 1, companyName: "Amazon" },
-        { id: 2, companyName: "Walmart" },
-        { id: 3, companyName: "Autozone" }
-      ]);
+  const [partners] = useState([
+    { id: 1, companyName: "Amazon" },
+    { id: 2, companyName: "Walmart" },
+    { id: 3, companyName: "Autozone" },
+  ]);
 
-    let pageName = "";
-    const icp = parseInt(currentPath.split("/")[2]);
-  
-    partners.map((partner) => {
-      if (currentPath === "/Admin") {
-        pageName = "Partners";
-      } else if (currentPath === "/Cliente") {
-        pageName = "PartnerShip";
+  let pageName = "";
+  const icp = parseInt(currentPath.split("/")[2]);
+
+  partners.map((partner) => {
+    if (currentPath === "/Admin") {
+      pageName = "Partners";
+    } else if (currentPath === "/Cliente") {
+      pageName = "PartnerShip";
+    }
+
+    if (icp > partners.length) {
+      return router.push("/404");
+    } else {
+      if (
+        currentPath === `/Cliente/${partner.id}` ||
+        currentPath === `/Admin/${partner.id}`
+      ) {
+        pageName = partner.companyName + " Partnership EDI Verification";
       }
-  
-      if (icp > partners.length) {
-        return router.push("/404");
-      } else {
-        if (currentPath === `/Cliente/${partner.id}` || currentPath === `/Admin/${partner.id}`) {
-          pageName = (partner.companyName + " Partnership EDI Verification");
-        }
-      }
-    });
-  
-    return pageName;
-  }
+    }
+  });
 
-export { getPageName };
+  return pageName;
+}
+
+export { useGetPageName };
