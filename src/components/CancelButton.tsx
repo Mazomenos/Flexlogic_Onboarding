eb354c11fcae6a6e2e27a25f65c3c21179221a18
NovoxMx <A00835132@tec.mx>
2024-05-20 18:19:24 -0700

    style: Update CancelButton size class to use fraction instead of fixed value

--- src/components/CancelButton.tsx
+++ src/components/CancelButton.tsx
@@ -1,7 +1,7 @@
 export default function CancelButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="w-fit p-2 h-10 bg-error text-black hover:bg-transparent hover:text-brand-red hover:ring-brand-red hover:ring-2 rounded-md flex justify-center items-center"
+      className="size-4/12 p-2 h-10 bg-error text-black hover:bg-transparent hover:text-brand-red hover:ring-brand-red hover:ring-2 rounded-md flex justify-center items-center"
       onClick={onClick}
     >
       <div> Cancel </div>
