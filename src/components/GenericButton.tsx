03ca848d6c15413c0071ae56f970f4c5b03da69a
NovoxMx <A00835132@tec.mx>
2024-05-20 18:19:36 -0700

    style: Update GenericButton size class to use fraction instead of fixed value

--- src/components/GenericButton.tsx
+++ src/components/GenericButton.tsx
@@ -1,6 +1,6 @@
 export default function GenericButton({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
     return (
-        <button className="size-1/12 h-10 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md" onClick={onClick}>
+        <button className="size-4/12 h-10 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md" onClick={onClick}>
             <div className="flex justify-center items-center"> {children} </div>
         </button>
     );
