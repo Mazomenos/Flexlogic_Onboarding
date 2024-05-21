adf7793ca3fa26695b2263b841295551c05489c7
mctuliomontoya <mctulio.2@gmail.com>
2024-05-20 18:13:27 -0700

    fix: change Badge.tsx route

--- src/app/(dashboard)/Cliente/[id]/page.tsx
+++ src/app/(dashboard)/Cliente/[id]/page.tsx
@@ -4,7 +4,7 @@ import BrakeRule from "@/components/BrakeRule";
 import ListItem from "@/components/ListItem";
 import { IoMdDownload } from "react-icons/io";
 import { TfiLayoutLineSolid } from "react-icons/tfi";
-import Badges from "../components/Badges";
+import Badge from "@/components/Badge";
 
 type Partnership = {
   id: number;
@@ -15,21 +15,49 @@ type Partnership = {
 
 export default function Home() {
   const partnerships: Partnership[] = [
-    { id: 1, EDIDoc: "EDI 850 Purchase Order", mandatory: true, status: "Validate" },
-    { id: 2, EDIDoc: "EDI 860 Purchase Order Change Request ", mandatory: false, status: "Validate" },
-    { id: 3, EDIDoc: "EDI 855 Purchase Order Acknowledgment", mandatory: true, status: "Complete" },
-    { id: 4, EDIDoc: "EDI 856 Ship Notice/Manifest", mandatory: true, status: "Validate" },
-    { id: 5, EDIDoc: "EDI 820 Payment Order/Remittance Advice", mandatory: false, status: "Failed" },
+    {
+      id: 1,
+      EDIDoc: "EDI 850 Purchase Order",
+      mandatory: true,
+      status: "Validate",
+    },
+    {
+      id: 2,
+      EDIDoc: "EDI 860 Purchase Order Change Request ",
+      mandatory: false,
+      status: "Validate",
+    },
+    {
+      id: 3,
+      EDIDoc: "EDI 855 Purchase Order Acknowledgment",
+      mandatory: true,
+      status: "Complete",
+    },
+    {
+      id: 4,
+      EDIDoc: "EDI 856 Ship Notice/Manifest",
+      mandatory: true,
+      status: "Validate",
+    },
+    {
+      id: 5,
+      EDIDoc: "EDI 820 Payment Order/Remittance Advice",
+      mandatory: false,
+      status: "Failed",
+    },
   ];
 
   const handleAddPartner = () => {
     console.log("Add Partnership");
-  }
+  };
 
   return (
     <div className="h-full flex flex-col">
       <div className="flex justify-end">
-        <AddButton onClick={() => handleAddPartner()}> Download PO Test <IoMdDownload /> </AddButton>
+        <AddButton onClick={() => handleAddPartner()}>
+          {" "}
+          Download PO Test <IoMdDownload />{" "}
+        </AddButton>
       </div>
       <BrakeRule />
       <div className="max-h-full flex flex-col items-center w-full overflow-y-auto overscroll-none">
@@ -37,12 +65,17 @@ export default function Home() {
           <ListItem key={index} path={partnership.EDIDoc}>
             <div className="flex flex-row w-full">
               <p className="basis-2/5">{partnership.EDIDoc} </p>
-              <TfiLayoutLineSolid style={{ transform: 'rotate(90deg)' }} className="grid content-center h-full" size={32} />
-              <p className="basis-2/5">{partnership.mandatory ? "Mandatory" : "Optional"} </p>
+              <TfiLayoutLineSolid
+                style={{ transform: "rotate(90deg)" }}
+                className="grid content-center h-full"
+                size={32}
+              />
+              <p className="basis-2/5">
+                {partnership.mandatory ? "Mandatory" : "Optional"}{" "}
+              </p>
               <div className="basis-1/5 flex justify-end">
-                <Badges status={partnership.status} />
+                <Badge status={partnership.status} />
               </div>
-
             </div>
           </ListItem>
         ))}
