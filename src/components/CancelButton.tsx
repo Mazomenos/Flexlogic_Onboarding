80c57d945edf7d4fbb7ff2806761e4d887f6b0e4
mctuliomontoya <mctulio.2@gmail.com>
2024-05-21 17:39:59 -0700

    style: change button w

--- src/components/CancelButton.tsx
+++ src/components/CancelButton.tsx
@@ -1,10 +1,10 @@
 export default function CancelButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="size-4/12 p-2 h-10 bg-error text-black hover:bg-transparent hover:text-brand-red hover:ring-brand-red hover:ring-2 rounded-md flex justify-center items-center"
+      className="p-2 w-36 h-10 bg-error text-black hover:bg-transparent hover:text-brand-red hover:ring-brand-red hover:ring-2 rounded-md"
       onClick={onClick}
     >
       <div> Cancel </div>
     </button>
-  );
+  )
 }
--- src/components/GenericButton.tsx
+++ src/components/GenericButton.tsx
@@ -1,7 +1,16 @@
-export default function GenericButton({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
-    return (
-        <button className="size-4/12 h-10 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md" onClick={onClick}>
-            <div className="flex justify-center items-center"> {children} </div>
-        </button>
-    );
+export default function GenericButton({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode
+  onClick: () => void
+}) {
+  return (
+    <button
+      className="h-10 w-36 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md"
+      onClick={onClick}
+    >
+      <div className=""> {children} </div>
+    </button>
+  )
 }
