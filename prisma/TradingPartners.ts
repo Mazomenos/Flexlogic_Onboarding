2030e4f6c8881c1d63410405a5ed71af17360c5b
mctuliomontoya <mctulio.2@gmail.com>
2024-05-23 13:03:57 -0700

    feat: add interface as type

--- prisma/TradingPartners.ts
+++ prisma/TradingPartners.ts
@@ -1,18 +1,19 @@
-export const TradingPartners = [
-    {
-        id: "664d76a8d7412ac29ddf6a1b",
-        Name: "Amazon",
-        Initial850EDI: "jejejeje",
-        Delimiters: ["*",">","~"],
-        EDIVersion: "X12 4010",
-        EOL: "CR",
-        DocsRequired: [
-            {
-                idDoc: "664d783c5d59cf13115aaec8",
-                Doc: "850",
-                Version: "X12 4010",
-                isVisible: false
-            }
-        ]
-    },
+import { TradingPartner } from "./interfaces/EDIInterfaces";
+export const TradingPartners: TradingPartner[] = [
+  {
+    id: "664d76a8d7412ac29ddf6a1b",
+    Name: "Amazon",
+    Initial850EDI: "jejejeje",
+    Delimiters: ["*", ">", "~"],
+    EDIVersion: "X12 4010",
+    EOL: "CR",
+    DocsRequired: [
+      {
+        idDoc: "664d783c5d59cf13115aaec8",
+        Doc: "850",
+        Version: "X12 4010",
+        isVisible: false,
+      },
+    ],
+  },
 ];
