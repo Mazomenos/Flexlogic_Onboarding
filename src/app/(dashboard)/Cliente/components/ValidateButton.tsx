7aa53b1964a802a92b3ffbc243d347e86cfc1e99
mctuliomontoya <mctulio.2@gmail.com>
2024-05-23 12:28:21 -0700

    style: button for validation

--- src/app/(dashboard)/Cliente/components/ValidateButton.tsx
+++ src/app/(dashboard)/Cliente/components/ValidateButton.tsx
@@ -7,7 +7,7 @@ export default function ValidateButton({
 }) {
   return (
     <button
-      className="h-10 w-28 bg-primary text-black hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md"
+      className="h-10 w-28 bg-info text-lg font-bold text-info-content transition motion-reduce:transition-none motion-reduce:hover:transform-none hover:bg-transparent hover:text-brand-blue hover:border-primary hover:border-2 rounded-md"
       onClick={onClick}
     >
       <div className=""> {children} </div>
