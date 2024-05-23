7d53c7ee394c828731a5060143d3dd4d9c16a4bf
NovoxMx <A00835132@tec.mx>
2024-05-22 18:59:52 -0700

    feat: Update Errors component to display error messages and export to id page

--- src/app/(dashboard)/Cliente/docs/Errors.tsx
+++ src/app/(dashboard)/Cliente/docs/Errors.tsx
@@ -1,15 +1,13 @@
+// Errors.tsx
 'use client'
 
-import { DialogTitle, Description } from '@headlessui/react'
+import { DialogTitle } from '@headlessui/react'
 import Modal from '@/components/Modal'
 import React from 'react'
 import { TfiLayoutLineSolid } from "react-icons/tfi";
 import CancelButton from '@/components/CancelButton'
 import ListItem from '@/components/ListItem'
 import BrakeRule from '@/components/BrakeRule'
-import BackButton from '@/components/BackButton'
-import AddButton from '@/components/AddButton'
-
 
 type Errors = {
     id: number
@@ -17,73 +15,42 @@ type Errors = {
     desc: string
 }
 
-export default function Errors() {
+export default function Errors({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
     const errors: Errors[] = [
-
-        {
-            id: 1,
-            typeError: 'Segmentacion',
-            desc: 'This is a mandatory document',
-        },
-        {
-            id: 2,
-            typeError: 'EDI 860 Purchase Order Change Request',
-            desc: 'This is a optional document',
-        },
-        {
-            id: 3,
-            typeError: 'EDI 855 Purchase Order Acknowledgment',
-            desc: 'This is a mandatory document',
-        },
-        {
-            id: 4,
-            typeError: 'EDI 856 Ship Notice/Manifest',
-            desc: 'This is a mandatory document',
-        },
-        {
-            id: 5,
-            typeError: 'EDI 810 Invoice',
-            desc: 'This is a mandatory document',
-        },
-        {
-            id: 6,
-            typeError: 'EDI 997 Functional Acknowledgment',
-            desc: 'This is a mandatory document',
-        },
-        {
-            id: 7,
-            typeError: 'EDI 860 Purchase Order Change Request',
-            desc: 'This is a optional document',
-        },
-    ]
-
-    const [isOpen, setIsOpen] = React.useState(false)
+        { id: 1, typeError: 'Segmentation', desc: 'This is a mandatory document' },
+        { id: 2, typeError: 'Elements', desc: 'This is a optional document' },
+        { id: 3, typeError: 'Segmentation', desc: 'This is a mandatory document' },
+        { id: 4, typeError: 'Elements', desc: 'This is a mandatory document' },
+        { id: 5, typeError: 'Elements', desc: 'This is a mandatory document' },
+        { id: 6, typeError: 'Segmentation', desc: 'This is a mandatory document' },
+        { id: 7, typeError: 'Elements', desc: 'This is a optional document' },
+    ];
 
     return (
-        <>
-            <div className='grid justify-items-end'>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+            <DialogTitle className="text-2xl">ERRORS</DialogTitle>
+            <BrakeRule classname="my-3" />
+            <div className="max-h-fit flex flex-col items-center w-full overflow-y-auto overscroll-none">
+                {errors.map((error) => (
+                    <ListItem key={error.id}>
+                        <div className='flex flex-row w-full'>
+                            <p className='basis-2/6'>{error.typeError}</p>
+                            <div className='basis-1/6'>
+                                <TfiLayoutLineSolid
+                                    style={{ transform: "rotate(90deg)" }}
+                                    className="grid content-center h-full"
+                                    size={32}
+                                />
+                            </div>
+                            <p className='basis-3/6'>{error.desc}</p>
+                        </div>
+                    </ListItem>
+                ))}
             </div>
-            <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-                <DialogTitle className="text-2xl">ERRORS</DialogTitle>
-                <BrakeRule classname="my-3" />
-                <div className="max-h-fit flex flex-col items-center w-full overflow-y-auto overscroll-none">
-                    {errors.map((errors) => (
-                        <ListItem key={errors.id}>
-                            <p> type: {errors.typeError} </p>
-                            <TfiLayoutLineSolid
-                                style={{ transform: "rotate(90deg)" }}
-                                className="grid content-center h-full"
-                                size={32}
-                            />
-                            <p>{errors.desc}</p>
-                        </ListItem>
-                    ))}
-                </div>
-                <BrakeRule classname="my-3" />
-                <div className="absolute bottom-2 w-full flex justify-center">
-                    <CancelButton onClick={() => setIsOpen(false)} />{' '}
-                </div>
-            </Modal>
-        </>
+            <BrakeRule classname="my-3" />
+            <div className="absolute bottom-2 w-full flex justify-center">
+                <CancelButton onClick={() => setIsOpen(false)} />{' '}
+            </div>
+        </Modal>
     )
 }
