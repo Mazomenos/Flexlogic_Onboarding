43b982bfdf50bcf9748144d508ec3b99f959c20f
NovoxMx <A00835132@tec.mx>
2024-05-23 18:53:04 -0700

    feat: Remove unused MyButton component

--- src/app/(dashboard)/Cliente/components/MyButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function MyButton({ children }: { children: React.ReactNode }) {
-  return (
-    <button className="size-1/12 h-10 bg-error text-black hover:bg-transparent hover:text-brand-red hover:border-brand-red hover:border-2 rounded-md flex justify-center items-center">
-      <div className="ml-2"> Cancel </div>
-    </button>
-  );
-}
