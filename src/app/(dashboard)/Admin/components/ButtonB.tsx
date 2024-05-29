7d0631f3eb03830b9708747c56c1c63be22764de
NovoxMx <A00835132@tec.mx>
2024-05-28 18:36:20 -0700

    feat: add ButtonB component for custom buttons Width

---
+++ src/app/(dashboard)/Admin/components/ButtonB.tsx
@@ -0,0 +1,18 @@
+interface ButtonBProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}
+
+export default function ButtonB({ children, onClick, className }: ButtonBProps) {
+  return (
+    <button
+      className={(
+        "text-base h-10 border-primary dark:border-darkMode-primary border-2 bg-info dark:bg-darkMode-primary text-info-content dark:text-darkMode-base-100 font-bold hover:bg-transparent dark:hover:bg-transparent hover:text-info-content/70 dark:hover:text-darkMode-primary hover:border-primary dark:hover:border-darkMode-primary transition motion-reduce:transition-none motion-reduce:hover:transform-none" + className
+      )}
+      onClick={onClick}
+    >
+      <div> {children} </div>
+    </button>
+  );
+}
