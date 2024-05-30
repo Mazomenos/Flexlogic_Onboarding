e9f384a748608b1e40f2aa946c91fb830b0be2bd
mctuliomontoya <mctulio.2@gmail.com>
2024-05-30 14:35:35 -0700

    change order of static partners

--- src/app/(dashboard)/utils/getPageName.tsx
+++ src/app/(dashboard)/utils/getPageName.tsx
@@ -16,8 +16,8 @@ function useGetPageName(currentPath: string) {
   const partnerships: Partnership[] = [
     { id: 1, name: "Amazon" },
     { id: 2, name: "Walmart" },
-    { id: 3, name: "Tufesa" },
-    { id: 4, name: "Amazon" },
+    { id: 3, name: "Autozone" },
+    { id: 4, name: "Tufesa" },
     { id: 5, name: "Walmart" },
     { id: 6, name: "Tufesa" },
     { id: 7, name: "Amazon" },
@@ -26,8 +26,8 @@ function useGetPageName(currentPath: string) {
   const partners: Partners[] = [
     { id: 1, companyName: "Amazon" },
     { id: 2, companyName: "Walmart" },
-    { id: 3, companyName: "Tufesa" },
-    { id: 4, companyName: "Amazon" },
+    { id: 3, companyName: "Autozone" },
+    { id: 4, companyName: "Tufesa" },
     { id: 5, companyName: "Walmart" },
     { id: 6, companyName: "Tufesa" },
     { id: 7, companyName: "Amazon" },
