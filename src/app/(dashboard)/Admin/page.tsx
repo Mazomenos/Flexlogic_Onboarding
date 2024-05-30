7697dbc6178fdca4fe3b73dd1bc3b8afac010aba
mctuliomontoya <mctulio.2@gmail.com>
2024-05-29 18:31:57 -0700

    feat: changed svg to icons

--- src/app/(dashboard)/Admin/page.tsx
+++ src/app/(dashboard)/Admin/page.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 import BrakeRule from "@/components/BrakeRule";
 import ListItem from "@/components/ListItem";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 import ActionsButton from "./components/ActionsButton";
 import Modal from "@/components/Modal";
 import { DialogTitle } from "@headlessui/react";
 import AddPartner from "./components/AddPartner";
+import { TrashIcon, EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -14,21 +14,6 @@ export default function Home() {
     number | null
   >(null);
   const [value, setValue] = React.useState("");
-
-  const handleDeleteButton = (id: number) => {
-    setSelectedPartnershipId(id);
-    setValue("");
-    setIsOpen(true);
-  };
-
-  function onSubmit() {
-    console.log("deleted");
-  }
-
-  const handleChange = (e: any) => {
-    setValue(e.target.value);
-  };
-
   const [partners, setPartners] = React.useState([
     {
       id: 1,
@@ -54,59 +39,25 @@ export default function Home() {
       ediDoc: ["850", "855", "810", "860"],
       visible: true,
     },
-    {
-      id: 4,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/mu6zkbsr9lfqeq9nvwls.png",
-      companyName: "Amazon",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
-    {
-      id: 5,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/dgf2ebuyc3xbrohuwzep.svg",
-      companyName: "Walmart",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
-    {
-      id: 6,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/oznp9xnvnzeclt8snqtr.png",
-      companyName: "Autozone",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
-    {
-      id: 7,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/mu6zkbsr9lfqeq9nvwls.png",
-      companyName: "Amazon",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
-    {
-      id: 8,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/dgf2ebuyc3xbrohuwzep.svg",
-      companyName: "Walmart",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
-    {
-      id: 9,
-      image:
-        "https://res.cloudinary.com/daoth80ez/image/upload/v1715896301/Media/oznp9xnvnzeclt8snqtr.png",
-      companyName: "Autozone",
-      ediDoc: ["850", "855", "810", "860"],
-      visible: true,
-    },
   ]);
 
   const selectedPartnership = partners.find(
     (partners) => partners.id === selectedPartnershipId,
   );
+
+  function onSubmit() {
+    console.log("deleted");
+  }
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleDeleteButton = (id: number) => {
+    setSelectedPartnershipId(id);
+    setValue("");
+    setIsOpen(true);
+  };
 
   const handleVisible = (id: number) => {
     setPartners((prevPartners) =>
@@ -116,10 +67,6 @@ export default function Home() {
     );
   };
 
-  const handleAddPartner = () => {
-    console.log("Add Partner");
-  };
-
   return (
     <div className="h-full flex flex-col ">
       <AddPartner />
@@ -127,8 +74,8 @@ export default function Home() {
       <div className="max-h-full flex flex-col items-center w-full overflow-y-auto overscroll-none">
         {partners.map((partner) => (
           <ListItem key={partner.id}>
-            <div className="flex flex-row w-full justify-center">
-              <div className="basis-1/12 grid justify-items-center content-center max-h-10 ">
+            <div className="flex flex-row w-full justify-center h-full">
+              <div className="basis-1/12 grid justify-items-center content-center h-full ">
                 <img
                   src={partner.image}
                   alt={partner.companyName}
@@ -146,12 +93,12 @@ export default function Home() {
                 onClick={() => handleVisible(partner.id)}
               >
                 {partner.visible ? (
-                  <FaEye size={24} />
+                  <EyeIcon className="size-8" />
                 ) : (
-                  <FaEyeSlash size={24} />
+                  <EyeSlashIcon className="size-8" />
                 )}
               </button>
-              <div className="basis-1/ grid justify-items-center content-center">
+              <div className="basis-1/12 grid justify-items-center content-center">
                 <ActionsButton
                   itemId={partner.id}
                   handleClick={handleDeleteButton}
@@ -191,20 +138,7 @@ export default function Home() {
             type="submit"
           >
             <div className="flex relative justify-center flex-row items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6 absolute left-2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
+              <TrashIcon className="size-6 absolute left-2" />
               Delete this partner
             </div>
           </button>
