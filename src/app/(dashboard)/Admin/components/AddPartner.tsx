77f1d43bc6e0cc4f5ef5a9a9574e74c1ac93246e
mctuliomontoya <mctulio.2@gmail.com>
2024-05-29 18:31:28 -0700

    feat: change inputs to handle darkmode & fix overscroll

--- src/app/(dashboard)/Admin/components/AddPartner.tsx
+++ src/app/(dashboard)/Admin/components/AddPartner.tsx
@@ -21,7 +21,7 @@ export default function AddPartner() {
             <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
                 <DialogTitle className="text-2xl">Create Partner</DialogTitle>
                 <BrakeRule classname="my-3" />
-                <div className="w-full">
+                <div className="w-full overflow-y-scroll">
                     <form className="space-y-6">
                         <div className="flex flex-col">
                             <label htmlFor="partnerName">
@@ -32,7 +32,7 @@ export default function AddPartner() {
                                 id="partnerName"
                                 name="partnerName"
                                 placeholder="Write the partner's name"
-                                className="p-2 border-b-2 border-[#515151] focus:outline-none focus:border-[#515151] focus:border-b-2"
+                                className="appearance-none p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-b-2"
                             />
                         </div>
                         <div className="flex flex-col">
@@ -44,7 +44,7 @@ export default function AddPartner() {
                                 id="urlImage"
                                 name="urlImage"
                                 placeholder="Write the URL of the image"
-                                className="p-2 border-b-2 border-[#515151] focus:outline-none focus:border-[#515151] focus:border-b-2"
+                                className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
                             />
                         </div>
                         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
@@ -55,7 +55,7 @@ export default function AddPartner() {
                                 <select
                                     id="delimiters"
                                     name="delimiters"
-                                    className="p-2 border-b-2 border-[#515151] focus:outline-none focus:border-[#515151] focus:border-b-2"
+                                    className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
                                 >
                                     <option value="">Choose delimiters</option>
                                     <option value=",">Comma (,)</option>
@@ -70,7 +70,7 @@ export default function AddPartner() {
                                 <select
                                     id="ediVersion"
                                     name="ediVersion"
-                                    className="p-2 border-b-2 border-[#515151] focus:outline-none focus:border-[#515151] focus:border-b-2"
+                                    className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
                                 >
                                     <option value="">Choose EDI version</option>
                                     <option value="4010">X12 4010</option>
@@ -85,7 +85,7 @@ export default function AddPartner() {
                                 <select
                                     id="eol"
                                     name="eol"
-                                    className="p-2 border-b-2 border-[#515151] focus:outline-none focus:border-[#515151] focus:border-b-2"
+                                    className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
                                 >
                                     <option value="">Choose EOL</option>
                                     <option value="LF"> ~ </option>
