bdce747c496cf16ff21afd9c48d32b55d20ca3a7
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-06-04 16:16:40 -0700

    conditions test placeholder

---
+++ __tests__/conditions.test.tsx
@@ -0,0 +1,16 @@
+import Conditions from "@/libs/validation/elements/conditions"
+import { Iconditions, IcharLength, IdecimalCondition, IcompareCodes, IcompareData, IdateAndTimeFormat, Operator } from "@/libs/validation/elements/conditions/InterfaceConditions"
+/*
+describe("Comparison Conditions", () => {
+    it("2 is less than 5", () => {
+        const compareData: IcompareData = {
+            source: 0,
+            data: {
+                operator: Operator.LESS,
+                data: "5"
+            }
+        }
+        
+    })
+})
+*/
