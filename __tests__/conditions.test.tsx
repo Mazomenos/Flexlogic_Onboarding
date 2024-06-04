4575eb4f573ce5683b37a916bf1abd86fa931957
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-06-04 16:52:48 -0700

    types tests and silent logs

--- __tests__/conditions.test.tsx
+++ __tests__/conditions.test.tsx
@@ -1,6 +1,6 @@
 import Conditions from "@/libs/validation/elements/conditions"
 import { Iconditions, IcharLength, IdecimalCondition, IcompareCodes, IcompareData, IdateAndTimeFormat, Operator } from "@/libs/validation/elements/conditions/InterfaceConditions"
-/*
+
 describe("Comparison Conditions", () => {
     it("2 is less than 5", () => {
         const compareData: IcompareData = {
@@ -10,7 +10,6 @@ describe("Comparison Conditions", () => {
                 data: "5"
             }
         }
-        
+        expect(true).toBe(true)
     })
 })
-*/
