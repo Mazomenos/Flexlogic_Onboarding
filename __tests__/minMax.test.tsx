6dc4016685cdcd2e67846187d2350cab4414ecf2
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-06-04 15:50:06 -0700

    typescript en jest

--- __tests__/minMax.test.tsx
+++ __tests__/minMax.test.tsx
@@ -1,37 +1,37 @@
-import { Min, Max } from "src/libs/validation/elements/minMax"
+import { Min, Max } from "@/libs/validation/elements/minMax"
 
 describe('MinMax: 2 - 10', () => {
     it('Below Min 2', () => {
         const input = "2"
-        const minRes = Min(input, 2)
+        const minRes = Min(input, 2, "")
         const maxRes = Max(input, 10)
         expect(minRes).toBe("Valor debe ser minimo 2")
         expect(maxRes).toBe("")
     })
     it('Min 2', () => {
         const input = "20"
-        const minRes = Min(input, 2)
+        const minRes = Min(input, 2, "")
         const maxRes = Max(input, 10)
         expect(minRes).toBe("")
         expect(maxRes).toBe("")
     })
     it('Mid Value', () => {
         const input = "202030"
-        const minRes = Min(input, 2)
+        const minRes = Min(input, 2, "")
         const maxRes = Max(input, 10)
         expect(minRes).toBe("")
         expect(maxRes).toBe("")
     })
     it('Max 10', () => {
         const input = "2020306610"
-        const minRes = Min(input, 2)
+        const minRes = Min(input, 2, "")
         const maxRes = Max(input, 10)
         expect(minRes).toBe("")
         expect(maxRes).toBe("")
     })
     it('Above Max 10', () => {
         const input = "2020306610X"
-        const minRes = Min(input, 2)
+        const minRes = Min(input, 2, "")
         const maxRes = Max(input, 10)
         expect(minRes).toBe("")
         expect(maxRes).toBe("Valor debe ser maximo 10")
