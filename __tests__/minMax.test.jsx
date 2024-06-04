bad1477d2aab229033b6417be9f2714ea390f50c
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-06-04 13:16:41 -0700

    Min max tests

--- __tests__/minMax.test.jsx
+++ __tests__/minMax.test.jsx
@@ -1,9 +1,39 @@
 import { Min, Max } from "src/libs/validation/elements/minMax"
 
-describe('Test', () => {
+describe('MinMax: 2 - 10', () => {
+    it('Below Min 2', () => {
+        const input = "2"
+        const minRes = Min(input, 2)
+        const maxRes = Max(input, 10)
+        expect(minRes).toBe("Valor debe ser minimo 2")
+        expect(maxRes).toBe("")
+    })
     it('Min 2', () => {
-        const res = Min("20", 2)
-
-        expect(res).toBe("")
+        const input = "20"
+        const minRes = Min(input, 2)
+        const maxRes = Max(input, 10)
+        expect(minRes).toBe("")
+        expect(maxRes).toBe("")
+    })
+    it('Mid Value', () => {
+        const input = "202030"
+        const minRes = Min(input, 2)
+        const maxRes = Max(input, 10)
+        expect(minRes).toBe("")
+        expect(maxRes).toBe("")
+    })
+    it('Max 10', () => {
+        const input = "2020306610"
+        const minRes = Min(input, 2)
+        const maxRes = Max(input, 10)
+        expect(minRes).toBe("")
+        expect(maxRes).toBe("")
+    })
+    it('Above Max 10', () => {
+        const input = "2020306610X"
+        const minRes = Min(input, 2)
+        const maxRes = Max(input, 10)
+        expect(minRes).toBe("")
+        expect(maxRes).toBe("Valor debe ser maximo 10")
     })
 })
