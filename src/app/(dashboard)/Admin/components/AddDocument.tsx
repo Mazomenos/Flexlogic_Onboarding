980561beb3a8c1cd63e0d09b85e34a8c0a4f34c6
NovoxMx <A00835132@tec.mx>
2024-06-03 17:44:36 -0700

    feat: Update AddDocument component to use dynamic options for EDI Document, Delimiters, and EOL

--- src/app/(dashboard)/Admin/components/AddDocument.tsx
+++ src/app/(dashboard)/Admin/components/AddDocument.tsx
@@ -8,6 +8,24 @@ import AddButton from "@/components/AddButton";
 import { useRouter } from "next/navigation";
 import ButtonB from "./ButtonB";
 
+
+const EdiDocument = [
+  { value: "", label: "Choose Document" },
+  { value: "856", label: "EDI 856" },
+  { value: "810", label: "EDI 810" },
+
+];
+
+const delimitersOptions = [
+  { value: ",", label: "Comma (,)" },
+  { value: ";", label: "Semicolon (;)" },
+  { value: "|", label: "Pipe (|)" }
+];
+
+const eolOptions = [
+  { value: "LF", label: " ~ " },
+];
+
 export default function AddDocument() {
   const router = useRouter();
 
@@ -25,30 +43,58 @@ export default function AddDocument() {
         <BrakeRule classname="my-3" />
         <div className="w-full overflow-y-scroll">
           <form className="space-y-6">
-            <div className="flex flex-col">
-              <label htmlFor="delimiters">Delimiters</label>
-              <select
-                id="delimiters"
-                name="delimiters"
-                className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
-              >
-                <option value="">Choose Document</option>
-                <option value="856">EDI 856</option>
-                <option value="810">EDI 810</option>
-              </select>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="delimiters">EDI Document</label>
+                <select
+                  id="delimiters"
+                  name="delimiters"
+                  className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
+                >
+                  {EdiDocument.map((item) => (
+                    <option value={item.value} className="dark:text-white dark:bg-darkMode-base-100">{item.label}</option>
+                  ))}
+
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="ediVersion"> Delimiters </label>
+                <select
+                  id="ediVersion"
+                  name="ediVersion"
+                  className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
+                >
+                  {delimitersOptions.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                      className="dark:text-white dark:bg-darkMode-base-100"
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="ediVersion"> EOL </label>
+                <select
+                  id="ediVersion"
+                  name="ediVersion"
+                  className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
+                >
+                  {eolOptions.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                      className="dark:text-white dark:bg-darkMode-base-100"
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
 
-            <div className="flex flex-col">
-              <label htmlFor="ediVersion">EDI Version</label>
-              <select
-                id="ediVersion"
-                name="ediVersion"
-                className="p-2 border-b-2 border-primary-content/50 dark:border-darkMode-foreground/60 dark:bg-transparent focus:border-blue-300 dark:focus:border-info-content focus:outline-none focus:border-primary-content/50 focus:border-b-2"
-              >
-                <option value="">Choose EDI version</option>
-                <option value="4010">X12 4010</option>
-              </select>
-            </div>
             <div className="flex justify-center">
               <ButtonB
                 onClick={() => setIsOpenForms(false)}
