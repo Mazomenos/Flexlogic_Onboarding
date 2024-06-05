a58bfbe4dc9c2be5002c9c002cccb1c826a5f336
mctuliomontoya <mctulio.2@gmail.com>
2024-06-05 10:14:01 -0700

    feat: create form modal

---
+++ src/app/(dashboard)/Admin/components/FormModal.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { CloseButton } from "@headlessui/react";
+
+interface Props {
+  children: React.ReactNode;
+  buttonText: string;
+}
+
+export default function FormModal({ children, buttonText }: Props) {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            className="w-56 h-12 bg-info dark:border-darkMode-primary dark:bg-darkMode-primary dark:hover:bg-transparent dark:text-darkMode-base-100 dark:hover:text-darkMode-primary text-lg font-semibold text-info-content hover:bg-transparent hover:text-info hover:border-info hover:border-2 shadow transition motion-reduce:transition-none motion-reduce:hover:transform-none"
+            variant="outline"
+          >
+            {buttonText}
+          </Button>
+        </DialogTrigger>
+        <DialogContent>{children}</DialogContent>
+      </Dialog>
+    </>
+  );
+}
