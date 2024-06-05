92b58d33b59ea0b2280c1b58732acf7d119e1c06
NovoxMx <A00835132@tec.mx>
2024-06-04 18:07:47 -0700

    feat: Add login functionality and navigation to client and admin pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
 import Image from "next/image";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import FlexLogicLogo from "@/components/FlexLogicLogo";
+import LoginButton from "@/components/LoginButton";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClient = () => {
+    router.push(`/Cliente/`);
+  }
+
+  const handleFlexlogic = () => {
+    router.push(`/Admin/`);
+  }
+
   return (
     <ThemeProvider
       attribute="class"
@@ -13,11 +26,11 @@ export default function Home() {
       <div className="bg-info w-screen h-screen dark:bg-darkMode-base-200">
         <div className="flex flex-col items-center justify-center h-full">
           <div className="mb-20 mt-32">
-            <FlexLogicLogo/>
+            <FlexLogicLogo />
           </div>
           <div className="flex justify-evenly w-full h-full">
 
-            <div className="w-2/5 lg:w-1/5 md:w-1/5 sm:w-1/3 h-3/5 lg:h-4/5 md:h-4/5 sm:h-3/4 bg-slate-100 rounded flex flex-col items-center p-4 justify-around dark:bg-darkMode-base-100">
+            <div className="w-2/5 xl:w-1/5 lg:w-2/5 md:w-2/5 sm:w-1/3 h-5/6 xl:h-4/5 lg:h-4/5 md:h-4/5 sm:h-3/4 bg-slate-100 rounded flex flex-col items-center p-4 justify-around dark:bg-darkMode-base-100 ">
               <div>
                 <p className="md:text-3xl sm:text-sm text-center mb-4 text-black dark:text-slate-100">
                   Welcome,
@@ -34,12 +47,12 @@ export default function Home() {
                   height={200}
                 />
               </div>
-              <button className="bg-[#FFCF52] w-full text-black p-2 rounded mt-4 md:text-lg sm:text-sm hover:bg-transparent dark:text-slate-100">
-                Continue
-              </button>
+              <div className="w-full">
+                <LoginButton onClick={() => handleFlexlogic()} />
+              </div>
             </div>
 
-            <div className="w-2/5 lg:w-1/5 md:w-1/5 sm:w-1/3 h-3/5 lg:h-4/5 md:h-4/5 sm:h-3/4 bg-slate-100 rounded flex flex-col items-center p-4 justify-around dark:bg-darkMode-base-100">
+            <div className="w-2/5 xl:w-1/5 lg:w-2/5 md:w-2/5 sm:w-1/3 h-5/6 xl:h-4/5 lg:h-4/5 md:h-4/5 sm:h-3/4 bg-slate-100 rounded flex flex-col items-center p-4 justify-around dark:bg-darkMode-base-100 ">
               <div>
                 <p className="md:text-3xl sm:text-sm text-center mb-4 text-black dark:text-slate-100">
                   Welcome,
@@ -56,15 +69,14 @@ export default function Home() {
                   height={200}
                 />
               </div>
-              <button className="bg-[#FFCF52] w-full text-black p-2 rounded mt-4 md:text-lg sm:text-sm dark:text-slate-100">
-                Continue
-              </button>
+              <div className="w-full">
+                <LoginButton onClick={() => handleClient()} />
+              </div>
             </div>
 
           </div>
         </div>
       </div>
     </ThemeProvider>
-
   );
 }
