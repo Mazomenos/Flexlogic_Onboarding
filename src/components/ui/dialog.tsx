510532aac9c25b420fbbf1e4f0a27858e6848649
mctuliomontoya <mctulio.2@gmail.com>
2024-06-05 16:26:00 -0700

    style: fix styles on base components

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -45,9 +45,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800 dark:data-[state=open]:text-slate-400">
-        <CloseButton onClick={() => {}} />
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ));
