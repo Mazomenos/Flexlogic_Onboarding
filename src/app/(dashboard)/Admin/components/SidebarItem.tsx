e03da67ce807af17d3ac5193bfec4fb6de1ff5fa
mctuliomontoya <mctulio.2@gmail.com>
2024-06-07 17:07:02 -0700

    style: add custom shadow to sidebar item

--- src/app/(dashboard)/Admin/components/SidebarItem.tsx
+++ src/app/(dashboard)/Admin/components/SidebarItem.tsx
@@ -15,20 +15,17 @@ import {
 } from "@/components/ui/select";
 import { ReactNode } from "react";
 import { MinusCircleIcon } from "@heroicons/react/24/outline";
-import { EDITemplateDocument } from "../../../../../prisma/interfaces/EDIInterfaces";
-import { EDIElement } from "../../../../../prisma/interfaces/EDIInterfaces";
-import { EDISegment } from "../../../../../prisma/interfaces/EDIInterfaces";
-import {EDITemplateDocs} from "../../../../../prisma/EDITemplateDocs";
-import {EDIElements} from "../../../../../prisma/EDIElements";
-import {EDISegments} from "../../../../../prisma/EDISegments";
+import { EDITemplateDocs } from "../../../../../prisma/EDITemplateDocs";
+import { EDIElements } from "../../../../../prisma/EDIElements";
+import { EDISegments } from "../../../../../prisma/EDISegments";
 
 export default function SidebarItem({ children }: { children?: ReactNode }) {
-
-
   return (
     <div className="my-5">
       {EDITemplateDocs[0].Segments.map((templateSegment) => {
-        const matchingSegment = EDISegments.find(segment => segment.Segment === templateSegment.Segment);
+        const matchingSegment = EDISegments.find(
+          (segment) => segment.Segment === templateSegment.Segment,
+        );
         if (!matchingSegment) return null;
 
         return (
@@ -36,26 +33,37 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
             key={templateSegment.Position}
             type="single"
             collapsible
-            className="mb-5 shadow-md max-w-full w-full collapse bg-base-100 pb-0 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
+            className="mb-5 shadow-custom max-w-full w-full collapse bg-base-100 pb-0 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
           >
-            <AccordionItem value={`item-${templateSegment.Position}`} className="pb-0 w-full">
+            <AccordionItem
+              value={`item-${templateSegment.Position}`}
+              className="pb-0 w-full"
+            >
               <AccordionTrigger className="text-lg">
                 <div className="px-2 flex w-full">
-                  <div className="basis-1/12 w-full flex justify-center self-center ">{templateSegment.Segment}</div>
-                  <div className="basis-7/12 overflow-hidden text-ellipsis whitespace-nowrap w-full flex self-center text-center">
+                  <div className="basis-1/12 flex-shrink-0 w-full flex justify-center self-center ">
+                    {templateSegment.Segment}
+                  </div>
+                  <div className="basis-7/12 flex-shrink overflow-hidden text-ellipsis whitespace-nowrap w-full flex self-center text-center">
                     {matchingSegment.Name}
                   </div>
-                  <div className="basis-3/12 w-full flex justify-center self-center ">
-                    {templateSegment.Requirement === "M" ? "Mandatory" : "Optional"}
+                  <div className="basis-3/12 flex-shrink-0 w-full flex justify-center self-center text-center">
+                    {templateSegment.Requirement === "M"
+                      ? "Mandatory"
+                      : "Optional"}
                   </div>
-                  <div className="basis-1/12 flex justify-center self-center">
-                    <MinusCircleIcon className={`h-7 w-7 ${templateSegment.Requirement === "M" ? "text-gray-300" : "text-darkMode-error-content"}`} />
+                  <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                    <MinusCircleIcon
+                      className={`h-7 w-7 ${templateSegment.Requirement === "M" ? "text-gray-300" : "text-darkMode-error-content"}`}
+                    />
                   </div>
                 </div>
               </AccordionTrigger>
               <AccordionContent className="pb-0">
                 {matchingSegment.Elements.map((element) => {
-                  const matchingElement = EDIElements.find(e => e.Element === element.Element);
+                  const matchingElement = EDIElements.find(
+                    (e) => e.Element === element.Element,
+                  );
                   if (!matchingElement) return null;
 
                   return (
@@ -68,15 +76,25 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
                       <AccordionItem value={`item-${element.Position}`}>
                         <AccordionTrigger className="text-md">
                           <div className="px-2 flex w-full">
-                            <div className="basis-2/12 w-full">{element.Position}</div>
-                            <div className="basis-5/12 overflow-hidden text-ellipsis whitespace-nowrap flex self-center">
+                            <div className="basis-2/12 flex-shrink-0 w-full">
+                              {element.Position}
+                            </div>
+                            <div className="basis-5/12 flex-shrink overflow-hidden text-ellipsis whitespace-nowrap flex self-center">
                               {matchingElement.Name}
                             </div>
-                            <div className="basis-1/12 flex justify-center self-center">{element.Requirement}</div>
-                            <div className="basis-1/12 flex justify-center self-center">{matchingElement.Min}</div>
-                            <div className="basis-1/12 flex justify-center self-center">{matchingElement.Max}</div>
-                            <div className="basis-1/12 flex justify-center self-center">
-                              <MinusCircleIcon className={`h-6 w-6 ${element.Requirement === "M" ? "text-gray-300" : "text-darkMode-error-content"}`} />
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {element.Requirement}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {matchingElement.Min}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {matchingElement.Max}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              <MinusCircleIcon
+                                className={`h-6 w-6 ${element.Requirement === "M" ? "text-gray-300" : "text-darkMode-error-content"}`}
+                              />
                             </div>
                           </div>
                         </AccordionTrigger>
@@ -92,10 +110,18 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
                                   <SelectGroup>
                                     <SelectLabel>Fruits</SelectLabel>
                                     <SelectItem value="apple">Apple</SelectItem>
-                                    <SelectItem value="banana">Banana</SelectItem>
-                                    <SelectItem value="blueberry">Blueberry</SelectItem>
-                                    <SelectItem value="grapes">Grapes</SelectItem>
-                                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                                    <SelectItem value="banana">
+                                      Banana
+                                    </SelectItem>
+                                    <SelectItem value="blueberry">
+                                      Blueberry
+                                    </SelectItem>
+                                    <SelectItem value="grapes">
+                                      Grapes
+                                    </SelectItem>
+                                    <SelectItem value="pineapple">
+                                      Pineapple
+                                    </SelectItem>
                                   </SelectGroup>
                                 </SelectContent>
                               </Select>
