ee619b6a2f6b6688ba87ae016ce206294170ee79
mctuliomontoya <mctulio.2@gmail.com>
2024-06-10 16:22:38 -0700

    conditions enum

---
+++ src/app/(dashboard)/Admin/enums/Conditions.tsx
@@ -0,0 +1,8 @@
+export enum Conditions {
+  CHARACTER_LENGTH = "Character Length",
+  DECIMAL_POSITION = "Decimals",
+  ID_CODES = "ID codes",
+  DATE_FORMAT = "Date Format",
+  TIME_FORMAT = "Time Format",
+  COMPARE = "Compare",
+}
