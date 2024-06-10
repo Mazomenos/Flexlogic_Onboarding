28ec5f1348cfc36bc4d1d7faeb9ea2b367946681
NovoxMx <A00835132@tec.mx>
2024-06-09 19:12:50 -0700

    fix: add collapsible icon to ErrorItem component

--- src/app/(dashboard)/Cliente/components/ErrorItem.tsx
+++ src/app/(dashboard)/Cliente/components/ErrorItem.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 interface IError {
   title: string;
@@ -7,23 +8,42 @@ interface IError {
 }
 
 export default function ErrorItem({ title, children, type }: IError) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <li className="list-none flex h-full justify-center">
       <div className="collapse bg-base-100 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 shadow-[0px_0px_10px_1px_#00000024] dark:shadow-[0px_0px_10px_1px_#dadee610] transition motion-reduce:transition-none motion-reduce:hover:transform-none p-2 rounded-none my-2 w-[97%]">
-        <input type="checkbox" />
-        <div className="collapse-title text-xl font-medium">
-          <p className="text-error-content/50 dark:text-darkMode-foreground/90">
+        <input 
+          type="checkbox" 
+          className="hidden" 
+          checked={isOpen} 
+          onChange={() => setIsOpen(!isOpen)} 
+        />
+        <div 
+          className="collapse-title text-xl font-medium flex justify-between items-center cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <p className="text-error-content/50 dark:text-darkMode-foreground/90 flex-1">
             <span className="font-bold text-error-content/80 dark:text-darkMode-error-content">
               {type} Error:
             </span>{" "}
-            {title}{" "}
+            {title}
           </p>
+          <div className="text-error-content/50 dark:text-darkMode-foreground">
+            {isOpen ? (
+              <ChevronDownIcon className="h-6 w-6 transform rotate-180 transition-transform duration-100" />
+            ) : (
+              <ChevronDownIcon className="h-6 w-6 transition-transform duration-100" />
+            )}
+          </div>
         </div>
-        <div className="collapse-content">
-          <p className="text-error-content/50 dark:text-darkMode-foreground">
-            {children}
-          </p>
-        </div>
+        {isOpen && (
+          <div className="collapse-content">
+            <p className="text-error-content/50 dark:text-darkMode-foreground">
+              {children}
+            </p>
+          </div>
+        )}
       </div>
     </li>
   );
