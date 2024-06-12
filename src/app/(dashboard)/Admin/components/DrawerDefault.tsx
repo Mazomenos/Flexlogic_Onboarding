ba097f5cd3776063f10bc3ecf0ae016a7aea8b03
mctuliomontoya <mctulio.2@gmail.com>
2024-06-12 14:38:36 -0700

    style: add style to buttons

--- src/app/(dashboard)/Admin/components/DrawerDefault.tsx
+++ src/app/(dashboard)/Admin/components/DrawerDefault.tsx
@@ -4,7 +4,10 @@ import CloseButton from "@/components/CloseButton";
 import GenericButton from "@/components/GenericButton";
 import { GetTPDocById } from "@/DA/TpDocsController";
 import { SuccessAction } from "@/components/toasters";
-
+import { DownloadIcon } from "@radix-ui/react-icons";
+import AddButton from "@/components/AddButton";
+import { IoMdCloudUpload, IoMdDownload } from "react-icons/io";
+import BrakeRule from "@/components/BrakeRule";
 
 interface Props {
   children?: React.ReactNode;
@@ -13,9 +16,6 @@ interface Props {
   idDocument: string;
   documentTitle: string | undefined;
 }
-
- 
-
 
 export default function DrawerDefault({
   open,
@@ -29,28 +29,23 @@ export default function DrawerDefault({
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
 
- 
-
   async function handleDownload() {
     try {
-      console.log("jeje")
-      SuccessAction("Download will Start Shortly")
-      const jsonData = await GetTPDocById(idDocument);    
+      console.log("jeje");
+      SuccessAction("Download will Start Shortly");
+      const jsonData = await GetTPDocById(idDocument);
       const encodedData = encodeURIComponent(JSON.stringify(jsonData));
       const href = `data:text/json;charset=utf-8,${encodedData}`;
-      const downloadLink = document.createElement('a');
+      const downloadLink = document.createElement("a");
       downloadLink.href = href;
       downloadLink.download = "filename.json";
       downloadLink.click();
     } catch (error) {
-      console.error('Error downloading JSON:', error);
-  };}
-
-  async function handleUpload() {
-    
+      console.error("Error downloading JSON:", error);
+    }
   }
 
-
+  async function handleUpload() {}
 
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
     const startX = e.clientX;
@@ -71,8 +66,8 @@ export default function DrawerDefault({
   };
 
   const YOURJSON = {
-    "Segments": "hello"
-  }
+    Segments: "hello",
+  };
 
   return (
     <Drawer
@@ -89,21 +84,41 @@ export default function DrawerDefault({
         </p>
         <CloseButton onClick={closeDrawer} />
       </div>
-      <div className=" px-3 flex justify-center gap-5">
-        <div className=" mt-2 flex ">
-          <GenericButton onClick={() => {}}>Upload </GenericButton>
+      <div className=" px-3 flex w-full flex-col items-center justify-center">
+        <div className=" mt-2 flex flex-col justify-center w-full items-center ">
+          <AddButton onClick={() => {}}>
+            Upload <IoMdCloudUpload className="ml-3" />
+          </AddButton>
+
+          <p className="text-lg mt-2 w-3/4 text-center italic">
+            Upload json file with document configuration
+          </p>
         </div>
-        <div className=" mt-2 flex justify-end">
-          <GenericButton onClick={() => {handleDownload()}}>Download </GenericButton>
+        <div className="inline-flex items-center justify-center w-full">
+          <hr className="w-64 h-px my-6 bg-gray-200 border-0 dark:bg-gray-700" />
+          <span className="absolute px-3 font-medium text-primary-content/50 bg-base-100 -translate-x-1/2  left-1/2 dark:text-darkMode-foreground/50 dark:bg-darkMode-base-200">
+            or
+          </span>
+        </div>
+
+        <div className=" mt-2 flex flex-col w-full justify-center items-center ">
+          <AddButton
+            onClick={() => {
+              handleDownload();
+            }}
+          >
+            Download <IoMdDownload />
+          </AddButton>
+
+          <p className="text-lg mt-2 w-3/4 text-center italic">
+            Download the current document configuration in json format
+          </p>
         </div>
       </div>
       <div
         className="absolute top-0 left-0 h-full w-2 cursor-ew-resize"
         onMouseDown={handleMouseDown}
       />
-      <div className="w-full mt-2 flex justify-end">
-        <GenericButton onClick={() => {}}>Create </GenericButton>
-      </div>
     </Drawer>
   );
 }
