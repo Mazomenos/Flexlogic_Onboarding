06357eff0345a8702a42a419d067ab22c2fbefe7
mctuliomontoya <mctulio.2@gmail.com>
2024-06-11 19:02:39 -0700

    fix: fixed confirmation input to show with spaces

--- src/app/(dashboard)/Admin/page.tsx
+++ src/app/(dashboard)/Admin/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
         <p>
           To confirm, please type &quot;
           <span className="font-bold select-none">
-            {selectedPartnership?.Name}
+            {selectedPartnership?.Name.split("_").join(" ")}
           </span>
           &quot; in the box below
         </p>
@@ -180,7 +180,7 @@ export default function Home() {
           <input
             value={value}
             onChange={handleChange}
-            placeholder={`Please type ${selectedPartnership?.Name} to confirm`}
+            placeholder={`Please type ${selectedPartnership?.Name.split("_").join(" ")} to confirm`}
             className="w-full outline-none text-lg placeholder:text-primary-content/40 dark:placeholder:text-darkMode-foreground/40 p-3 bg-inherit border-2 rounded-md appearance-none border-error dark:border-darkMode-error focus:bg-base-200 dark:focus:bg-darkMode-base-200"
             type="text"
             name="name"
@@ -188,7 +188,12 @@ export default function Home() {
           <div className="my-3" />
           <button
             className="disabled:bg-base-300/30 dark:disabled:bg-darkMode-base-300/30 dark:disabled:border-darkMode-foreground/30 disabled:cursor-not-allowed disabled:text-primary-content/30 dark:disabled:text-darkMode-foreground/30 font-bold border-2 enabled:border-error dark:enabled:border-darkMode-error enabled:hover:bg-transparent dark:enabled:hover:bg-transparent enabled:text-error-content/70 dark:enabled:text-darkMode-error-content enabled:bg-error dark:enabled:bg-darkMode-error h-12 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded"
-            disabled={value === selectedPartnership?.Name ? false : true}
+            disabled={
+              value.split("_").join(" ") ===
+              selectedPartnership?.Name.split("_").join(" ")
+                ? false
+                : true
+            }
             onClick={deleteDatabasePartner}
           >
             <div className="flex relative justify-center flex-row items-center">
