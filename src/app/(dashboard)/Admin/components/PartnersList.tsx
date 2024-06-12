be0931a383ef5a5dfcc57ab2b2bd1fa1848a2bda
mctuliomontoya <mctulio.2@gmail.com>
2024-06-11 18:51:36 -0700

    pop up when visible change

--- src/app/(dashboard)/Admin/components/PartnersList.tsx
+++ src/app/(dashboard)/Admin/components/PartnersList.tsx
@@ -3,12 +3,15 @@ import PartnerItem from "./PartnerItem";
 import { TradingPartnerCard } from "../page";
 import { updateTPDoc } from "@/DA/TpDocsController";
 import { UpdateTradingPartner } from "@/DA/tradingPartnerControllers";
+import { SuccessAction } from "@/components/toasters";
 interface Props {
   Partners: TradingPartnerCard[];
   handleDeleteButton: any;
   handleEditButton: any;
-  temporalPartners: TradingPartnerCard[]
-  setTemporalPartners: React.Dispatch<React.SetStateAction<TradingPartnerCard[]>>
+  temporalPartners: TradingPartnerCard[];
+  setTemporalPartners: React.Dispatch<
+    React.SetStateAction<TradingPartnerCard[]>
+  >;
   handleDeleteTemporalPartner: (id: string) => void;
 }
 
@@ -18,13 +21,26 @@ export default function PartnersList({
   handleDeleteButton,
   temporalPartners,
   setTemporalPartners,
-  handleDeleteTemporalPartner
+  handleDeleteTemporalPartner,
 }: Props) {
-
-  const handleUpdatePartner = async (updatedPartner: TradingPartnerCard) => {
+  const handleUpdatePartner = async (
+    updatedPartner: TradingPartnerCard,
+    type: string,
+  ) => {
     const newPartners = temporalPartners.map((partner) =>
       partner.id === updatedPartner.id ? updatedPartner : partner,
     );
+    let change = "";
+
+    if (type === "visible") {
+      if (updatedPartner.isVisible === true) {
+        change = "Visible";
+      } else {
+        change = "Not Visible";
+      }
+    }
+
+    SuccessAction(`Switched ${updatedPartner.Name} to ${change}`);
     setTemporalPartners(newPartners);
   };
 
