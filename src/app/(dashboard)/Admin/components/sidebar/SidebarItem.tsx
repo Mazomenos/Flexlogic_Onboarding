416ce2d470be0488f3d4f104a2df4a454b473e40
NovoxMx <A00835132@tec.mx>
2024-06-11 21:27:20 -0700

    test: now Loop are visible in the sidebar

--- src/app/(dashboard)/Admin/components/sidebar/SidebarItem.tsx
+++ src/app/(dashboard)/Admin/components/sidebar/SidebarItem.tsx
@@ -44,7 +44,7 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
     setIsModalOpen(false);
   };
 
-  const addSegment = (newSegment: EDISegment) => {
+  const addSegment = (newSegment) => {
     const updatedSegments = [...EDITemplate, newSegment];
     setEDITemplate(updatedSegments);
     closeModal();
@@ -62,7 +62,7 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
       (element) => element.Element !== elementToRemove
     );
     setElements(updatedElements);
-  
+
     const updatedTemplate = EDITemplate.map((segment) => {
       const matchingSegment = EDISegments.find(
         (s) => s.Segment === segment.Segment
@@ -71,22 +71,192 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
       const updatedElements = matchingSegment.Elements.filter(
         (element) => element.Element !== elementToRemove
       );
-  
+
       return {
         ...segment,
         Elements: updatedElements,
       };
     });
-    
+
     console.log(updatedTemplate);
     setEDITemplate(updatedTemplate);
   };
-  
-
 
   const usedSegments = EDITemplate.map(
     (templateSegment) => templateSegment.Segment,
   );
+
+  const renderSegments = (segments) => {
+    return segments.map((templateSegment) => {
+      if (templateSegment.Segment === "Loop") {
+        return (
+          <Accordion
+            key={templateSegment.Position}
+            type="single"
+            collapsible
+            className="w-[98%] mb-3 mt-2 shadow-custom  collapse bg-base-100 pb-0 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
+          >
+            <AccordionItem
+              value={`item-${templateSegment.Position}`}
+              className="pb-0 w-full"
+            >
+              <AccordionTrigger className="text-lg">
+                <div className="px-2 flex w-full">
+                  <div className="basis-1/12 flex-shrink-0 w-full flex justify-center self-center ">
+                    {templateSegment.Segment}
+                  </div>
+                  <span className="flex-shrink flex self-center basis-7/12 overflow-hidden text-ellipsis whitespace-nowrap">
+                    {templateSegment.Requirement}
+                  </span>
+                  <div className="basis-3/12 flex-shrink-0 w-full flex justify-center self-center text-center">
+                    {templateSegment.Max}
+                  </div>
+                  <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                    <MinusCircleIcon
+                      className={`h-7 w-7 ${templateSegment.Requirement === "M"
+                        ? "text-gray-300"
+                        : "text-darkMode-error-content cursor-pointer"
+                        }`}
+                      onClick={() => {
+                        if (templateSegment.Requirement !== "M") {
+                          removeSegment(templateSegment.Segment);
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="pb-0">
+                {renderSegments(templateSegment.Segments)}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        );
+      }
+
+      const matchingSegment = EDISegments.find(
+        (segment) => segment.Segment === templateSegment.Segment,
+      );
+      if (!matchingSegment) return null;
+
+      return (
+        <div key={templateSegment.Position} className="flex justify-center">
+          <Accordion
+            key={templateSegment.Position}
+            type="single"
+            collapsible
+            className="w-[98%] mb-3 mt-2 shadow-custom  collapse bg-base-100 pb-0 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
+          >
+            <AccordionItem
+              value={`item-${templateSegment.Position}`}
+              className="pb-0 w-full"
+            >
+              <AccordionTrigger className="text-lg">
+                <div className="px-2 flex w-full">
+                  <div className="basis-1/12 flex-shrink-0 w-full flex justify-center self-center ">
+                    {templateSegment.Segment}
+                  </div>
+                  <span className="flex-shrink flex self-center basis-7/12 overflow-hidden text-ellipsis whitespace-nowrap">
+                    {matchingSegment.Name}
+                  </span>
+                  <div className="basis-3/12 flex-shrink-0 w-full flex justify-center self-center text-center">
+                    {templateSegment.Requirement === "M"
+                      ? "Mandatory"
+                      : "Optional"}
+                  </div>
+                  <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                    <MinusCircleIcon
+                      className={`h-7 w-7 ${templateSegment.Requirement === "M"
+                        ? "text-gray-300"
+                        : "text-darkMode-error-content cursor-pointer"
+                        }`}
+                      onClick={() => {
+                        if (templateSegment.Requirement !== "M") {
+                          removeSegment(templateSegment.Segment);
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="pb-0">
+                {matchingSegment.Elements.map((element, index) => {
+                  const matchingElement = EDIElements.find(
+                    (e) => e.Element === element.Element,
+                  );
+                  if (!matchingElement) return null;
+
+                  return (
+                    <Accordion
+                      key={element.Position}
+                      type="single"
+                      collapsible
+                      className="w-full collapse bg-base-200 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
+                    >
+                      <AccordionItem
+                        value={`item-${templateSegment.Segment}-${element.Position}`}
+                        className="pb-0 w-full"
+                      >
+                        <AccordionTrigger className="text-md">
+                          <div className="px-2 flex w-full">
+                            <div className="basis-2/12 flex-shrink-0 w-full">
+                              {element.Position}
+                            </div>
+                            <div className="basis-5/12 flex-shrink overflow-hidden text-ellipsis whitespace-nowrap flex self-center">
+                              {matchingElement.Name}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {element.Requirement}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {matchingElement.Min}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              {matchingElement.Max}
+                            </div>
+                            <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
+                              <MinusCircleIcon
+                                className={`h-6 w-6 ${element.Requirement === "M"
+                                  ? "text-gray-300"
+                                  : "text-darkMode-error-content"
+                                  }`}
+                                onClick={() => {
+                                  if (element.Requirement !== "M") {
+                                    removeElement(element.Element);
+                                  }
+                                }}
+                              />
+                            </div>
+                          </div>
+                        </AccordionTrigger>
+
+                        <AccordionContent className="flex mt-2 justify-center">
+                          <ConditionSelector
+                            index={index}
+                            selection={
+                              conditionSelections[
+                              `element-${element.Position}`
+                              ] || ""
+                            }
+                            onSelectionChange={(value) =>
+                              handleSelectionChange(
+                                `element-${element.Position}`,
+                                value,
+                              )
+                            }
+                          />
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  );
+                })}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="flex flex-col h-full">
@@ -97,130 +267,7 @@ export default function SidebarItem({ children }: { children?: ReactNode }) {
         addSegment={addSegment}
       />
       <div className="my-5 no-scrollbar flex-1 overflow-y-auto overscroll-none">
-        {EDITemplate.map((templateSegment) => {
-          const matchingSegment = EDISegments.find(
-            (segment) => segment.Segment === templateSegment.Segment,
-          );
-          if (!matchingSegment) return null;
-
-          return (
-            <div key={templateSegment.Position} className="flex justify-center">
-              <Accordion
-                key={templateSegment.Position}
-                type="single"
-                collapsible
-                className="w-[98%] mb-3 mt-2 shadow-custom  collapse bg-base-100 pb-0 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
-              >
-                <AccordionItem
-                  value={`item-${templateSegment.Position}`}
-                  className="pb-0 w-full"
-                >
-                  <AccordionTrigger className="text-lg">
-                    <div className="px-2 flex w-full">
-                      <div className="basis-1/12 flex-shrink-0 w-full flex justify-center self-center ">
-                        {templateSegment.Segment}
-                      </div>
-                      <span className="flex-shrink flex self-center basis-7/12 overflow-hidden text-ellipsis whitespace-nowrap">
-                        {matchingSegment.Name}
-                      </span>
-                      <div className="basis-3/12 flex-shrink-0 w-full flex justify-center self-center text-center">
-                        {templateSegment.Requirement === "M"
-                          ? "Mandatory"
-                          : "Optional"}
-                      </div>
-                      <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
-                        <MinusCircleIcon
-                          className={`h-7 w-7 ${templateSegment.Requirement === "M"
-                            ? "text-gray-300"
-                            : "text-darkMode-error-content cursor-pointer"
-                            }`}
-                          onClick={() => {
-                            if (templateSegment.Requirement !== "M") {
-                              removeSegment(templateSegment.Segment);
-                            }
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </AccordionTrigger>
-                  <AccordionContent className="pb-0">
-                    {matchingSegment.Elements.map((element, index) => {
-                      const matchingElement = EDIElements.find(
-                        (e) => e.Element === element.Element,
-                      );
-                      if (!matchingElement) return null;
-
-                      return (
-                        <Accordion
-                          key={element.Position}
-                          type="single"
-                          collapsible
-                          className="w-full collapse bg-base-200 dark:bg-darkMode-base-100 hover:bg-base-200 dark:hover:bg-darkMode-base-200 transition motion-reduce:transition-none motion-reduce:hover:transform-none rounded-none"
-                        >
-                          <AccordionItem
-                            value={`item-${templateSegment.Segment}-${element.Position}`}
-                            className="pb-0 w-full"
-                          >
-                            <AccordionTrigger className="text-md">
-                              <div className="px-2 flex w-full">
-                                <div className="basis-2/12 flex-shrink-0 w-full">
-                                  {element.Position}
-                                </div>
-                                <div className="basis-5/12 flex-shrink overflow-hidden text-ellipsis whitespace-nowrap flex self-center">
-                                  {matchingElement.Name}
-                                </div>
-                                <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
-                                  {element.Requirement}
-                                </div>
-                                <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
-                                  {matchingElement.Min}
-                                </div>
-                                <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
-                                  {matchingElement.Max}
-                                </div>
-                                <div className="basis-1/12 flex-shrink-0 flex justify-center self-center">
-                                  <MinusCircleIcon
-                                    className={`h-6 w-6 ${element.Requirement === "M"
-                                      ? "text-gray-300"
-                                      : "text-darkMode-error-content"
-                                      }`}
-
-                                    onClick={() => {
-                                      if (element.Requirement !== "M") {
-                                        removeElement(element.Element);
-                                      }
-                                    }}
-                                  />
-                                </div>
-                              </div>
-                            </AccordionTrigger>
-
-                            <AccordionContent className="flex mt-2 justify-center">
-                              <ConditionSelector
-                                index={index}
-                                selection={
-                                  conditionSelections[
-                                  `element-${element.Position}`
-                                  ] || ""
-                                }
-                                onSelectionChange={(value) =>
-                                  handleSelectionChange(
-                                    `element-${element.Position}`,
-                                    value,
-                                  )
-                                }
-                              />
-                            </AccordionContent>
-                          </AccordionItem>
-                        </Accordion>
-                      );
-                    })}
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          );
-        })}
+        {renderSegments(EDITemplate)}
       </div>
     </div>
   );
